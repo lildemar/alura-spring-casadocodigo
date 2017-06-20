2c9c8ba8604f491b1cdba266757fd1152f93730d
Lildemar Matias dos Santos <lildemar@gmail.com>
2017-06-19 23:21:54 -0300

    Estudo de templates com spring mvc

--- src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -24,8 +24,8 @@ public class JPAConfiguration {
 
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setUsername("root");
-		dataSource.setPassword("root");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
+		dataSource.setPassword("");
+		dataSource.setUrl("jdbc:mysql://localhost:3309/casadocodigo");
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 
 		factoryBean.setDataSource(dataSource);
